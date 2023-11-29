fb22e75c8b395cfaec70789d655fba004d8ff7a3
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-29 19:49:01 -0300

    feat: create form structure and style

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Meus components
+import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HeaderComponent, FormsComponent],
+  exports: [HeaderComponent, FormsComponent],
+})
+export class AppModule {}

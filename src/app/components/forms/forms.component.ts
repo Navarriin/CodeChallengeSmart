4b57410a76064e36a6c4b4bff21f018669e46c87
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-30 16:10:16 -0300

    feat: add formGroup

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
+  result = [];
+  formGroup: FormGroup = new FormGroup({
+    hour: new FormControl(),
+    showClosed: new FormControl(),
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
 }

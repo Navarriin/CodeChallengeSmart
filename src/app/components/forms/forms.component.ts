c9e3cda0ec51e9831a1eaf6ad9e8f5d3bf4bac93
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-01 15:41:54 -0300

    feat: extracting the filter for a new service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { GetUnitsService } from '../../services/get-units.service';
+// Minha interface
 import { Location } from '../../types/units-response.interface';
-
-const OPENING_HOURS = {
-  morning: {
-    first: '06',
-    last: '12',
-  },
-  afternoon: {
-    first: '12',
-    last: '18',
-  },
-  night: {
-    first: '18',
-    last: '23',
-  },
-};
-
-type HOUR_INDEXES = 'morning' | 'afternoon' | 'night';
+// Meus services
+import { GetUnitsService } from '../../services/get-units.service';
+import { FilterUnitsService } from '../../services/filter-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -36,7 +22,10 @@ export class FormsComponent {
     showClosed: new FormControl(true),
   });
 
-  constructor(private unitService: GetUnitsService) {}
+  constructor(
+    private unitService: GetUnitsService,
+    private filterUnitsService: FilterUnitsService
+  ) {}
 
   ngOnInit(): void {
     this.unitService.getAllUnits().subscribe((data) => {
@@ -45,78 +34,17 @@ export class FormsComponent {
     });
   }
 
-  transformWeekday(weekday: number): string {
-    switch (weekday) {
-      case 0:
-        return 'Dom.';
-      case 6:
-        return 'Sáb.';
-      default:
-        return 'Seg. à Sex.';
-    }
-  }
-
-  filterUnits(unit: Location, open_hour: string, close_hour: string): boolean {
-    if (!unit.schedules) return true;
-
-    let open_hour_filter = parseInt(open_hour, 10);
-    let close_hour_filter = parseInt(close_hour, 10);
-
-    let today_weekdays = this.transformWeekday(new Date().getDay());
-
-    for (let i = 0; i < unit.schedules.length; i++) {
-      let schedule_hour = unit.schedules[i].hour;
-      let schedule_weekday = unit.schedules[i].weekdays;
-
-      if (today_weekdays === schedule_weekday) {
-        if (schedule_hour !== 'Fechada') {
-          let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ');
-          let unit_open_hour_int = parseInt(
-            unit_open_hour.replace('h', ''),
-            10
-          );
-          let unit_close_hour_int = parseInt(
-            unit_close_hour.replace('h', ''),
-            10
-          );
-          if (
-            unit_open_hour_int <= open_hour_filter &&
-            unit_close_hour_int >= close_hour_filter
-          ) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      }
-    }
-    return false;
-  }
-
   onSubmit(): void {
-    let intermediateResults = this.results;
-
-    if (!this.formGroup.value.showClosed) {
-      intermediateResults = this.results.filter(
-        (location) => location.opened === true
-      );
-    }
-
-    if (this.formGroup.value.hour) {
-      const OPEN_HOUR =
-        OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].first;
-      const CLOSE_HOUR =
-        OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].last;
-
-      this.filteredResults = intermediateResults.filter((location) =>
-        this.filterUnits(location, OPEN_HOUR, CLOSE_HOUR)
-      );
-    } else {
-      this.filteredResults = this.results;
-    }
+    let { showClosed, hour } = this.formGroup.value;
+    this.filteredResults = this.filterUnitsService.filter(
+      this.results,
+      showClosed,
+      hour
+    );
   }
 
   onClean(): void {
     this.formGroup.reset();
+    this.filteredResults = this.results;
   }
 }

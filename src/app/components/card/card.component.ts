5aefa76d0ff2cf7ab6a9d57b2f63eeeef51ad600
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-02 14:41:59 -0300

    feat: created card-list with content dinamic

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Location } from '../../types/units-response.interface';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  @Input() card!: Location;
+}
--- src/app/components/cards-list/cards-list.component.ts
+++ src/app/components/cards-list/cards-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Location } from '../../types/units-response.interface';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './cards-list.component.html',
   styleUrl: './cards-list.component.scss',
 })

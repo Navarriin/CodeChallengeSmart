f10aebd6df4e7936eb37c130b8cbfc595fb4a16b
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-02 15:53:32 -0300

    feat: add a legend component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Meus components
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { LegendComponent } from './components/legend/legend.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HeaderComponent, FormsComponent, CardsListComponent],
-  exports: [HeaderComponent, FormsComponent, CardsListComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FormsComponent,
+    CardsListComponent,
+    LegendComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    FormsComponent,
+    CardsListComponent,
+    LegendComponent,
+  ],
 })
 export class AppModule {}
